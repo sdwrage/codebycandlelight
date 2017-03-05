1345cfe64cd86e017be8cb163a1d31331ea975f1
Justin Lonas <sdwrage@gmail.com>
2017-03-05 03:13:51 -0500

    Added welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,5 +9,10 @@
 @endsection
 
 @section ('content')
-    <p>Welcome Home Justin :)</p>
+    <p>Well hello there! My name is Justin Lonas and I love long walks on the... you know what? Screw that. I love walking off the
+    beaten path. I love to discover things that most people probably have never even touched. My pride and joy is the experience I have with my family and friends and I hope to carry that on until my last breath.</p>
+
+    <p>What do I do? I am a Web Application Developer who loves both Front-end and Back-end Development. My main backend focus on PHP with experience in frameworks like Zend Framework 2 and Laravel. I also have experience in Ruby (Ruby on Rails) and Groovy (Grails). My Front-end focus is mainly on HTML5, CSS3, Twitter Bootstrap (as a base css framework), JQuery and ReactJS.</p>
+
+    <p>My intesterests? Poetry, Paranormal Research, Writing, Game Development and Fathering. I am open to developing new skills and experiencing new hobbies. Want to keep in touch? Contact me at <a href="mailto:justinlonas@me.com">justinlonas@me.com</a>.</p>
 @endsection
