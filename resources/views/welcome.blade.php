c86475205cd2ee5e686cbc8769e7dc28ee493828
Justin Lonas <sdwrage@gmail.com>
2017-03-05 12:26:47 -0500

    adding strong wording

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,12 +9,12 @@
 @endsection
 
 @section ('content')
-    <p>Well hello there! My name is Justin Lonas and I love long walks on the... you know what? Screw that. I love walking off the
+    <p><strong>Well hello there!</strong> My name is Justin Lonas and I love long walks on the... you know what? Screw that. I love walking off the
     beaten path. I love to discover things that most people probably have never even touched. My pride and joy is the experience I have with my family and friends and I hope to carry that on until my last breath.</p>
 
-    <p>What do I do? I am a Web Application Developer who loves both Front-end and Back-end Development. My main backend focus is on PHP with experience in PHP frameworks like Zend Framework 2 and Laravel. I also have experience in Ruby (Ruby on Rails) and Groovy (Grails). My Front-end focus is mainly on HTML5, CSS3, Twitter Bootstrap (as a base css framework), JQuery and ReactJS.</p>
+    <p><strong>What do I do?</strong> I am a Web Application Developer who loves both Front-end and Back-end Development. My main backend focus is on PHP with experience in PHP frameworks like Zend Framework 2 and Laravel. I also have experience in Ruby (Ruby on Rails) and Groovy (Grails). My Front-end focus is mainly on HTML5, CSS3, Twitter Bootstrap (as a base css framework), JQuery and ReactJS.</p>
 
-    <p>My interests? Poetry, Paranormal Research, Writing, Game Development and being the father to an amazing son. I am open to developing new skills and experiencing new hobbies. Want to keep in touch? Contact me at <a href="mailto:justinlonas@me.com">justinlonas@me.com</a>.</p>
+    <p><strong>My interests?</strong> Poetry, Paranormal Research, Writing, Game Development and being the father to an amazing son. I am open to developing new skills and experiencing new hobbies. Want to keep in touch? Contact me at <a href="mailto:justinlonas@me.com">justinlonas@me.com</a>.</p>
 
     <p>✊</p>
 @endsection
