1c8722e0eb2ba915f15edaea587dd98b1a625341
Justin Lonas <sdwrage@gmail.com>
2017-03-05 03:38:28 -0500

    Modified father section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
 
     <p>What do I do? I am a Web Application Developer who loves both Front-end and Back-end Development. My main backend focus is on PHP with experience in PHP frameworks like Zend Framework 2 and Laravel. I also have experience in Ruby (Ruby on Rails) and Groovy (Grails). My Front-end focus is mainly on HTML5, CSS3, Twitter Bootstrap (as a base css framework), JQuery and ReactJS.</p>
 
-    <p>My intesterests? Poetry, Paranormal Research, Writing, Game Development and Fathering. I am open to developing new skills and experiencing new hobbies. Want to keep in touch? Contact me at <a href="mailto:justinlonas@me.com">justinlonas@me.com</a>.</p>
+    <p>My intesterests? Poetry, Paranormal Research, Writing, Game Development and being the father to an amazing son. I am open to developing new skills and experiencing new hobbies. Want to keep in touch? Contact me at <a href="mailto:justinlonas@me.com">justinlonas@me.com</a>.</p>
 
     <p>✊</p>
 @endsection
